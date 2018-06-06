82acce76a2f9c9d86fe0e8e63b91759a244aff08
chasingw <wangd101@163.com>
2018-06-06 15:01:43 +0800

    add pcl1.8 for ubuntu 16.04

--- ros.sh
+++ ros.sh
@@ -2,6 +2,8 @@
 参考
 - [didi](https://github.com/udacity/didi-competition/blob/master/docs/GettingStarted.md)
 This tutorial covers installing ROS Indigo on Ubuntu 14.04. These versions work well with Autoware, an open-source self-driving car project built on top of ROS. Later on, Autoware will be useful for camera/LIDAR calibration.
+## todo
+add 
 
 1. Setup your sources.list
 ```
@@ -9,6 +11,10 @@ sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main
 ```
 2. Set up your keys
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+
+```
+sudo apt-get update	
+``` 
 3. Install ROS(this will take a few minutes)
 ```
 sudo apt-get install ros-indigo-desktop-full
